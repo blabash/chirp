5ae6d908aebcf27a07e298839e41b3eb3a504f47
Brett Labash <brett.labash@gmail.com>
2023-09-07 17:03:16 +0900

    Changes Install dayjs Update nextjs config for nextjs Image component Add PostView component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,30 @@
-import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignOutButton, useUser } from "@clerk/nextjs";
+
 import Head from "next/head";
-import Link from "next/link";
+import Image from "next/image";
+
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 import { api } from "~/utils/api";
 
+import type { RouterOutputs } from "~/utils/api";
+
+dayjs.extend(relativeTime);
+
 const CreatePostWizard = () => {
   const { user } = useUser();
-
-  console.log(user);
 
   if (!user) return null;
 
   return (
     <div className="flex w-full gap-3">
-      <img
+      <Image
         src={user.imageUrl}
         alt="Profile image"
-        className="h-16 w-16 rounded-full"
+        className="rounded-full"
+        width={56}
+        height={56}
       />
       <input
         type="text"
@@ -24,6 +32,32 @@ const CreatePostWizard = () => {
         className="grow bg-transparent outline-none"
       />
     </div>
+  );
+};
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+
+const PostView = (props: PostWithUser) => {
+  const { post, author } = props;
+  return (
+    <li className="flex items-center gap-3 border-b border-slate-400 p-4">
+      <Image
+        src={author.profilePicture}
+        alt="Profile image"
+        className="rounded-full"
+        width={56}
+        height={56}
+      />
+      <div className="flex flex-col">
+        <div className="flex text-slate-300">
+          <span>{`@${author.username}`}</span>
+          <span className="whitespace-pre font-thin">{`  ~ ${dayjs(
+            post.createdAt,
+          ).fromNow()}`}</span>
+        </div>
+        <span>{post.content}</span>
+      </div>
+    </li>
   );
 };
 
@@ -56,10 +90,8 @@ export default function Home() {
             )}
           </div>
           <ul className="flex flex-col">
-            {data.map(({ post, author }) => (
-              <li key={post.id} className="border-b border-slate-400 p-8">
-                {post.content}
-              </li>
+            {data.map((fullPost) => (
+              <PostView {...fullPost} key={fullPost.post.id} />
             ))}
           </ul>
         </div>
